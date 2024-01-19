3045c6ac3562e925b4e80062d8ec5b4df190e578
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-01-19 13:48:32 +0100

    .....

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -46,7 +46,26 @@ let favFoods = ['Pizza', 'Ice Cream', 'Sushi', 'Chocolate', 'Burgers'];
 // Modify the HTML element with the id of food to display your favorite foods array.
 foodElement.innerHTML = favFoods
 
+// Declare and instantiate a variable to hold another single favorite food item.
+let newFavoriteFood = 'Pasta';
 
+// Add the value stored in this new variable to your favorite food array.
+favFoods.push(newFavoriteFood);
+
+// Append the new array values onto the displayed content of the HTML element with the id of food.
+foodElement.innerHTML += `<br>${favFoods.join('<br>')}`;
+
+// Remove the first element in the favorite food array.
+favFoods.shift();
+
+// Append the array output showing the modified array, using a line break.
+foodElement.innerHTML += `<br>${favFoods.join('<br>')}`;
+
+// Remove the last element in the favorite food array.
+favFoods.pop();
+
+// Append the array output with this final modified favorite foods array.
+foodElement.innerHTML += `<br>${favFoods.join('<br>')}`;
 
 
 
