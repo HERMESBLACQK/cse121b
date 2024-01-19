8ec80e6dec2d44d1597f700a183ba1de4560be3c
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-01-19 15:02:00 +0100

    ,,,,,

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -7,44 +7,23 @@ const fullName = 'Omotoso Taiwo Henry';
 
 const currentYear = new Date().getFullYear();
 
-const profilePicture = 'images/taiwo.jpg'; 
-
-
-
+const profilePicture = 'images/taiwo.jpg';
 
 /* Step 3 - Element Variables */
-
 const nameElement = document.getElementById('name');
-
 const foodElement = document.getElementById('food');
-
-
 const yearElement = document.querySelector('#year');
 
-
-
-
 /* Step 4 - Adding Content */
-
-
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-
 yearElement.textContent = currentYear;
 
-// imageElement.setAttribute('src', profilePicture);
-
-
-
-
-
-
 /* Step 5 - Array */
-
 // Declare an array variable to hold your favorite foods.
-let favFoods = ['Pizza', 'Ice Cream', 'Sushi', 'Chocolate', 'Burgers'];
+let favFoods = ['Pizza', 'Ice Cream', 'Sushi', 'Chocoate', 'Burgers'];
 
 // Modify the HTML element with the id of food to display your favorite foods array.
-foodElement.innerHTML = favFoods
+foodElement.innerHTML = favFoods.join();
 
 // Declare and instantiate a variable to hold another single favorite food item.
 let newFavoriteFood = 'Pasta';
@@ -53,20 +32,16 @@ let newFavoriteFood = 'Pasta';
 favFoods.push(newFavoriteFood);
 
 // Append the new array values onto the displayed content of the HTML element with the id of food.
-foodElement.innerHTML += `<br>${favFoods.join}`;
+foodElement.innerHTML += `<br>${favFoods.join()}`;
 
 // Remove the first element in the favorite food array.
 favFoods.shift();
 
 // Append the array output showing the modified array, using a line break.
-foodElement.innerHTML += `<br>${favFoods.join}`;
+foodElement.innerHTML += `<br>${favFoods.join()}`;
 
 // Remove the last element in the favorite food array.
 favFoods.pop();
 
 // Append the array output with this final modified favorite foods array.
-foodElement.innerHTML += `<br>${favFoods.join}`;
-
-
-
-
+foodElement.innerHTML += `<br>${favFoods.join()}`;
