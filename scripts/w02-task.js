10b5fb2652841673c7a1cb94f19c16b9672e4697
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-01-19 13:47:27 +0100

    ....

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,12 +3,23 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+const fullName = 'Omotoso Taiwo Henry';
+
+const currentYear = new Date().getFullYear();
+
+const profilePicture = 'images/taiwo.jpg'; // Replace with the actual image name
 
 
 
 
 /* Step 3 - Element Variables */
 
+const nameElement = document.getElementById('name');
+
+let foodElement = document.getElementById('food');
+
+
+const yearElement = document.querySelector('#year');
 
 
 
@@ -16,11 +27,25 @@
 /* Step 4 - Adding Content */
 
 
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
+yearElement.textContent = currentYear;
+
+imageElement.setAttribute('src', profilePicture);
+
+
 
 
 
 
 /* Step 5 - Array */
+
+// Declare an array variable to hold your favorite foods.
+let favFoods = ['Pizza', 'Ice Cream', 'Sushi', 'Chocolate', 'Burgers'];
+
+// Modify the HTML element with the id of food to display your favorite foods array.
+foodElement.innerHTML = favFoods
+
 
 
 
