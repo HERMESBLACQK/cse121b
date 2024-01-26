9e7c6521da183452fae443535cfb82ed92e71be4
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-01-26 23:21:14 +0100

    ....

--- w03-task.js
+++ w03-task.js
@@ -20,7 +20,7 @@ function Subtract(number1, number2) {
           return number1 - number2;
       }
       
-      function addNumbers() {
+      function SubtractNumbers() {
           let addNumber1 = Number(document.querySelector('#add1').value);
           let addNumber2 = Number(document.querySelector('#add2').value);
           document.querySelector('#sum').value = add(addNumber1, addNumber2);
