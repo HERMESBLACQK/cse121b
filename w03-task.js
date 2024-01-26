5e89ae18a38dc397955a6b3133c6c8463f8f8a00
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-01-26 23:19:29 +0100

    ...

---
+++ w03-task.js
@@ -0,0 +1,84 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add(number1, number2) {
+          return number1 + number2;
+      }
+      
+      function addNumbers() {
+          let addNumber1 = Number(document.querySelector('#add1').value);
+          let addNumber2 = Number(document.querySelector('#add2').value);
+          document.querySelector('#sum').value = add(addNumber1, addNumber2);
+      }
+      
+      document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+
+/* Function Expression - Subtract Numbers */
+function add(number1, number2) {
+          return number1 + number2;
+      }
+      
+      function addNumbers() {
+          let addNumber1 = Number(document.querySelector('#add1').value);
+          let addNumber2 = Number(document.querySelector('#add2').value);
+          document.querySelector('#sum').value = add(addNumber1, addNumber2);
+      }
+      
+      document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+
+/* Arrow Function - Multiply Numbers */
+function add(number1, number2) {
+          return number1 + number2;
+      }
+      
+      function addNumbers() {
+          let addNumber1 = Number(document.querySelector('#add1').value);
+          let addNumber2 = Number(document.querySelector('#add2').value);
+          document.querySelector('#sum').value = add(addNumber1, addNumber2);
+      }
+      
+      document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+
+/* Open Function Use - Divide Numbers */
+function add(number1, number2) {
+          return number1 + number2;
+      }
+      
+      function addNumbers() {
+          let addNumber1 = Number(document.querySelector('#add1').value);
+          let addNumber2 = Number(document.querySelector('#add2').value);
+          document.querySelector('#sum').value = add(addNumber1, addNumber2);
+      }
+      
+      document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+document.querySelector('#array').textContent = `Array: ${numbersArray.join(', ')}`;
+
+
+/* Output Odds Only Array */
+document.querySelector('#odds').textContent = `Odds: ${numbersArray.filter(number => number % 2 !== 0).join(', ')}`;
+
+
+/* Output Evens Only Array */
+document.querySelector('#evens').textContent = `Evens: ${numbersArray.filter(number => number % 2 === 0).join(', ')}`;
+
+
+/* Output Sum of Org. Array */
+document.querySelector('#sumOfArray').textContent = `Sum: ${numbersArray.reduce((sum, number) => sum + number, 0)}`;
+
+
+/* Output Multiplied by 2 Array */
+
+
+/* Output Sum of Multiplied by 2 Array */
