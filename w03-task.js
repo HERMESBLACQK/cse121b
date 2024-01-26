44e2bca3893fe37d94bab48e388749a3e56a6d51
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-01-26 23:23:36 +0100

    ....

--- w03-task.js
+++ w03-task.js
@@ -21,9 +21,9 @@ function Subtract(number1, number2) {
       }
       
       function SubtractNumbers() {
-          let SubtractNumber1 = Number(document.querySelector('#add1').value);
-          let SubtractNumber2 = Number(document.querySelector('#add2').value);
-          document.querySelector('#sum').value = Subtract(SubtractNumber1, SubtractNumber2);
+          let subtractNumber1 = Number(document.querySelector('#add1').value);
+          let subtractNumber2 = Number(document.querySelector('#add2').value);
+          document.querySelector('#sum').value = Subtract(subtractNumber1, subtractNumber2);
       }
       
       document.querySelector('#SubtractNumbers').addEventListener('click', addNumbers);
