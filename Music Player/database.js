9aab8041931ec7e1d6e7816ad26576f2d51b3f65
HermesBlacqk <omotosolilhenxy@gmail.com>
2024-02-14 13:55:51 +0100

    ............

---
+++ Music Player/database.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name:"Did I?",
+        path:"songs/Lágbájá - Did I.mp3",
+        artist:"Lagbaja",
+        cover:""
+    },
+    {
+        name:"Iyawo Tororo",
+        path:"songs/Lagbaja - Iyawo TororoGimme 5.mp3",
+        artist:"Lagbaja",
+        cover:""
+    },
+    {
+        name:"Prayer for the Youth",
+        path:"songs/Lagbaja - Prayer for the Youth.mp3",
+        artist:"Lagbaja",
+        cover:""
+    },
+    {
+        name:"Simple Yes Or No",
+        path:"songs/Lagbaja - Simple Yes Or No.mp3",
+        artist:"Lagbaja",
+        cover:""
+    },
+    {
+        name:"Never Far Away",
+        path:"songs/Lagbaja And Ego Never Far Away.mp3",
+        artist:"Lagbaja And Ego",
+        cover:""
+    }
+]
